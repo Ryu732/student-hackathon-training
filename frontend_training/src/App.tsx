b037cc94e8d51641069187dd77faa3f50c95e785
Ryu732 <sakanaction001@gmail.com>
2025-02-13 22:53:37 +0900

    TODO完了機能

--- frontend_training/src/App.tsx
+++ frontend_training/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { useState } from 'react'
+import { v4 as uuid } from 'uuid'
 
 function App() {
 	return (
@@ -11,8 +12,9 @@ function App() {
 }
 
 type Todo = {
-	id: number;
+	id: string;
 	text: string;
+	isComplete: boolean;
 }
 
 function InputTodo() {
@@ -22,10 +24,22 @@ function InputTodo() {
 	function addTodo(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
 		setTodos([...todos, {
-			id: todos.length + 1,	// ID
+			id: uuid(),	// ID
 			text: text,	// 入力されたテキスト
+			isComplete: false,	// 完了フラグ
 		}]);
 		setText('');
+	}
+
+	function completeTodo(id: string) {
+		// TODOリストを完了済みにする
+		setTodos(todos.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, isComplete: true };
+			} else {
+				return todo;
+			}
+		}));
 	}
 
 	return (
@@ -35,8 +49,11 @@ function InputTodo() {
 				<button type="submit" className='addButton'>追加</button>
 			</form>
 			<ul>
-				{todos.map((todo) => (
-					<li key={todo.id}>{todo.text}</li>
+				{todos.filter(todo => (!todo.isComplete)).map((todo) => (
+					<div key={todo.id} className='todoItem'>
+						<li>{todo.text}</li>
+						<button onClick={() => (completeTodo(todo.id))} className='completeButton'>完了</button>
+					</div>
 				))}
 			</ul>
 		</>

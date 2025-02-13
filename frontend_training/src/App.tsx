2a91f0cebfc545faaf64e6d36c25c219bd66b3d4
Ryu732 <sakanaction001@gmail.com>
2025-02-14 01:12:10 +0900

    TODO編集機能

--- frontend_training/src/App.tsx
+++ frontend_training/src/App.tsx
@@ -6,27 +6,32 @@ function App() {
 	return (
 		<>
 			<h1>TODOアプリ</h1>
-			<InputTodo />
+			<Todo />
 		</>
 	)
 }
 
+// TODOリストの型定義
 type Todo = {
-	id: string;
-	text: string;
-	isComplete: boolean;
+	id: string; // ID(unique)
+	text: string; // TODOの内容
+	isComplete: boolean; // 完了フラグ
+	isEdit: boolean; // 編集フラグ
 }
 
-function InputTodo() {
+function Todo() {
 	const [text, setText] = useState<string>(''); // 入力されたテキストを保持するstate
 	const [todos, setTodos] = useState<Todo[]>([]); // TODOリストを保持するstate
 
 	function addTodo(e: React.FormEvent<HTMLFormElement>) {
+		// TODOリストに追加する
+		if (!text.trim()) return; // 入力が空の場合は何もしない
 		e.preventDefault();
 		setTodos([...todos, {
-			id: uuid(),	// ID
-			text: text,	// 入力されたテキスト
-			isComplete: false,	// 完了フラグ
+			id: uuid(),
+			text: text,
+			isComplete: false,
+			isEdit: false
 		}]);
 		setText('');
 	}
@@ -42,20 +47,83 @@ function InputTodo() {
 		}));
 	}
 
+	function startEdit(id: string) {
+		// 編集を開始する
+		setTodos(todos.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, isEdit: true };
+			} else {
+				return todo;
+			}
+		}));
+	}
+
+	function updateTodoText(text: string, id: string) {
+		// TODOリストを編集する
+		setTodos(todos.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, text: text, isEdit: false };
+			} else {
+				return todo;
+			}
+		}));
+	}
+
+	function closeEdit(id: string) {
+		// 編集を終了する
+		setTodos(todos.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, isEdit: false };
+			} else {
+				return todo;
+			}
+		}));
+	}
+
 	return (
 		<>
-			<form className='addForm' onSubmit={addTodo}>
-				<input type="text" className='addText' value={text} onChange={(e) => setText(e.target.value)} />
+			<form onSubmit={addTodo} className='addForm' >
+				<input type="text" value={text} onChange={(e) => setText(e.target.value)} className='addText' />
 				<button type="submit" className='addButton'>追加</button>
 			</form>
-			<ul>
+			<ul >
 				{todos.filter(todo => (!todo.isComplete)).map((todo) => (
-					<div key={todo.id} className='todoItem'>
-						<li>{todo.text}</li>
-						<button onClick={() => (completeTodo(todo.id))} className='completeButton'>完了</button>
+					<div key={todo.id} >
+						<div className='todoItem'>
+							<li onClick={() => startEdit(todo.id)}>{todo.text}</li>
+							<button onClick={() => (completeTodo(todo.id))} className='completeButton'>完了</button>
+						</div>
+						{todo.isEdit && <EditTodo todoText={todo.text} id={todo.id} updateTodoText={updateTodoText} closeEdit={closeEdit} />}
 					</div>
 				))}
 			</ul>
+		</>
+	)
+}
+
+type EditTodoProps = {
+	todoText: string;
+	id: string;
+	updateTodoText: (text: string, id: string) => void;
+	closeEdit: (id: string) => void;
+}
+
+function EditTodo({ todoText, id, updateTodoText, closeEdit }: EditTodoProps) {
+	const [text, setText] = useState<string>(todoText); // TODOの編集内容を保持するstate
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => { // 編集内容を保存する
+		e.preventDefault();
+		if (!text.trim()) return;
+		updateTodoText(text, id);
+	};
+
+	return (
+		<>
+			<form onSubmit={handleSubmit} className='editForm'>
+				<input type='text' value={text} onChange={(e) => setText(e.target.value)} className='editText'></input>
+				<button type='submit' className='updateButton'>更新</button>
+				<button type='button' onClick={() => closeEdit(id)} className='editCancelButton'>キャンセル</button>
+			</form>
 		</>
 	)
 }
